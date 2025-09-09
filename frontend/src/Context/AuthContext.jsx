7b4f13a3797996d4155b10b7c6ecf1de7ab6497d
Divya Rochani <119523148+divyarochani@users.noreply.github.com>
2025-09-09 20:19:46 +0530

    Update AuthContext.jsx

--- frontend/src/Context/AuthContext.jsx
+++ frontend/src/Context/AuthContext.jsx
@@ -1,7 +1,7 @@
 import React, { createContext, useState } from 'react'
 export const authDataContext = createContext()
 function AuthContext({children}) {
-    const serverUrl = "http://localhost:8080"
+    const serverUrl = "https://mehman-airbnb-repo.onrender.com"
 
     let [loading,setLoading]=useState(false)
 
