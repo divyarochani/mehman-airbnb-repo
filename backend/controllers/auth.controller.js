23dcd299900af9fc54b759032b3b36c791771395
hiteshmujwani <hiteshmujwaniofficial@gmail.com>
2025-09-27 00:26:39 +0530

    .

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -51,11 +51,7 @@ export const login = async (req, res) => {
 };
 export const logOut = async (req, res) => {
   try {
-    res.clearCookie("token", {
-      httpOnly: true,
-      secure: process.env.NODE_ENV === "production",
-      sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
-    });
+    res.cookie("token", "");
     return res.status(200).json({ message: "Logout Successfully" });
   } catch (error) {
     return res.status(500).json({ message: `logout error ${error}` });
