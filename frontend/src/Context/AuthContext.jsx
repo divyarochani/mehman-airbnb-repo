596957363a0459a830cacdbc84375d9ded2b0170
hiteshmujwani <hiteshmujwaniofficial@gmail.com>
2025-09-26 23:50:18 +0530

    cors error fixed

--- frontend/src/Context/AuthContext.jsx
+++ frontend/src/Context/AuthContext.jsx
@@ -1,21 +1,23 @@
-import React, { createContext, useState } from 'react'
-export const authDataContext = createContext()
-function AuthContext({children}) {
-    const serverUrl = "https://mehman-airbnb-repo.onrender.com"
+import React, { createContext, useState } from "react";
+export const authDataContext = createContext();
+function AuthContext({ children }) {
+  const serverUrl = "https://mehman-airbnb-repo.onrender.com"; // for server which is deployed on render
+  // const serverUrl = "http://localhost:8080"; // for local and change ip address according yours
 
-    let [loading,setLoading]=useState(false)
+  let [loading, setLoading] = useState(false);
 
-    let value={
-        serverUrl,
-        loading,setLoading
-    }
+  let value = {
+    serverUrl,
+    loading,
+    setLoading,
+  };
   return (
     <div>
-     <authDataContext.Provider value={value}>
+      <authDataContext.Provider value={value}>
         {children}
-     </authDataContext.Provider>
+      </authDataContext.Provider>
     </div>
-  )
+  );
 }
 
-export default AuthContext
+export default AuthContext;
